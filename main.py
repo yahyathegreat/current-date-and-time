291480cc23dd92f6691c9a4dde245d4a01ea5f45
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-10 15:13:52 +0500

    something

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ now = datetime.now()
 print("today's date is", today)
 print("\current date and time is", now)
 print("\ndate components",today.year, today.month, today.day)
+import random
+import time
+def getRandomDate( startDate, endDate):
+    print("print random  date between", startDate, "and", endDate)
+    randomGenerator = random.random()
+    dateformat = '%m/%d/%Y'
+    startTime = time.mktime(time.strptime(startDate, dateformat))
+    endTime = time.mktime(time.strptime(startDate,dateformat))
+    randomTime = startTime +  randomGenerator * (endTime - startTime)
+    randomDate = time.strftime(dateformat,time.localtime(randomTime))
+    return randomDate
+print("random date =", getRandomDate("2/3/2017","1/6/2024"))
